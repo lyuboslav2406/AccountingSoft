f836ba402b75c397bcd80d802c9b8e4997a69efc
lyuboslav2406 <56050277+lyuboslav2406@users.noreply.github.com>
2020-04-12 20:02:01 +0300

    Add controllers and views. Initial actions.

---
+++ Web/AccountingSoft.Web/Controllers/ClientController.cs
@@ -0,0 +1,36 @@
+﻿namespace AccountingSoft.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ClientController : Controller
+    {
+        public ClientController()
+        {
+        }
+
+        public IActionResult ById()
+        {
+            return this.View();
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        public IActionResult Delete()
+        {
+            return this.View();
+        }
+
+        public IActionResult Update()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ Web/AccountingSoft.Web/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿namespace AccountingSoft.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ProductController : Controller
+    {
+        public IActionResult All()
+        {
+            return this.View();
+        }
+
+        public IActionResult ByClient()
+        {
+            return this.View();
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        public IActionResult Delete()
+        {
+            return this.View();
+        }
+
+        public IActionResult Update()
+        {
+            return this.View();
+        }
+    }
+}
