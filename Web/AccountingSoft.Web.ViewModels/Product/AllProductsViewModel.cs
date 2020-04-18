a724a08c27e318b347156fc47058f034e869f5fc
lyuboslav2406 <56050277+lyuboslav2406@users.noreply.github.com>
2020-04-18 19:17:08 +0300

    add dropDown view model

---
+++ Web/AccountingSoft.Web.ViewModels/Product/AllProductsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountingSoft.Web.ViewModels.Product
+{
+    public class AllProductsViewModel
+    {
+        public IEnumerable<ProductViewModel> Clients { get; set; }
+    }
+}
---
+++ Web/AccountingSoft.Web.ViewModels/Product/ClientDropDownViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AccountingSoft.Web.ViewModels.Product
+{
+    using AccountingSoft.Data.Models;
+
+    using AccountingSoft.Services.Mapping;
+
+    public class ClientDropDownViewModel : IMapFrom<Client>
+    {
+        public System.Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
